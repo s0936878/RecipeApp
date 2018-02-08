a0bdb030edc2194769cef0d92986ffd22a7ed120
Patrice Case <s0936878@hh305-23ml56165.monmouth.edu>
2018-02-08 10:38:29 -0500

    Initial Commit

---
+++ RecipeApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RecipeApp
+//
+//  Created by Patrice K. Case on 2/8/18.
+//  Copyright © 2018 Patrice K. Case. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
