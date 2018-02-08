7fd44d690aa549fd7a84b2bad8df4539f0b224a4
Patrice Case <s0936878@hh305-23ml56165.local>
2018-02-08 10:47:47 -0500

    testest

--- RecipeApp/ViewController.swift
+++ RecipeApp/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 //test
+    //test
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
